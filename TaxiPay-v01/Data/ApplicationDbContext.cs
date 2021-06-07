c2e22f20a90dc1834ae4f456fba68521b1310c65
Cristóvão Quiombo <cristovao.quiombo@hotmail.com>
2021-06-07 15:49:38 +0100

    Criação dos projectos: Web e Mobile

---
+++ TaxiPay-v01/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxiPay_v01.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
